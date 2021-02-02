21d51bf7f0d9eb0b1c0342a7b0e59b24abd22abb
lmichelet <41468814+lmichelet@users.noreply.github.com>
2021-02-02 15:41:32 +0100

    Add files via upload

---
+++ bar.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="bar" tilewidth="32" tileheight="32" tilecount="96" columns="8">
+ <image source="bar.png" width="256" height="384"/>
+</tileset>
